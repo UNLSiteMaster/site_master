bef3de9d0a55cf9a8ce6bc6a2c497d22ef929bb2
mfairchild365 <mfairchild365@gmail.com>
2013-11-18 20:23:43 -0600

    add versioned update to the plugin interface

--- src/SiteMaster/Plugin/PluginInterface.php
+++ src/SiteMaster/Plugin/PluginInterface.php
@@ -1,6 +1,8 @@
 <?php
 namespace SiteMaster\Plugin;
 
+use SiteMaster\Plugin\PluginManager;
+
 abstract class PluginInterface
 {
     protected $options = array();
@@ -25,11 +27,30 @@ abstract class PluginInterface
     abstract public function onUnInstall();
 
     /**
+     * Called when the plugin is updated (a newer version exists).
+     *
+     * @param $previousVersion int The previous installed version
+     * @return mixed
+     */
+    abstract public function onUpdate($previousVersion);
+
+    /**
      * Returns the long name of the plugin
      *
      * @return mixed
      */
     abstract public function getName();
+
+    /**
+     * Returns the version of this plugin
+     * Follow a mmddyyyyxx syntax.
+     *
+     * for example 1118201301
+     * would be 11/18/2013 - increment 1
+     *
+     * @return mixed
+     */
+    abstract public function getVersion();
 
     /**
      * Returns a description of the plugin
@@ -91,8 +112,42 @@ abstract class PluginInterface
 
     public function isInstalled()
     {
-        if (!$plugin = Plugin::getByName($this->getMachineName())) {
+        $plugins = PluginManager::getManager()->getExternalPlugins();
+
+        if (!isset($plugins[$this->getName()])) {
             return false;
+        }
+
+        return true;
+    }
+
+    public function getInstalledVersion()
+    {
+        $plugins = PluginManager::getManager()->getExternalPlugins();
+
+        if (!isset($plugins[$this->getName()])) {
+            return false;
+        }
+
+        return $plugins[$this->getName()];
+    }
+
+    public function update()
+    {
+        if (!$this->isInstalled()) {
+            return $this->install();
+        }
+
+        //do we need to update?
+        $installedVersion = $this->getInstalledVersion();
+        if ($installedVersion >= $this->getVersion()) {
+            return false;
+        }
+
+        if ($this->onUpdate($installedVersion)) {
+            $plugins = PluginManager::getManager()->getExternalPlugins();
+            $plugins[$this->getName()] = $this->getVersion();
+            PluginManager::getManager()->updateInstalledPlugins($plugins);
         }
 
         return true;
