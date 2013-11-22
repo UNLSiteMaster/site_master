db077ea4cb00ac1bf8e4e5488e4b8979ac6abf98
mfairchild365 <mfairchild365@gmail.com>
2013-11-21 19:08:15 -0600

    refactor and preform uninstalls

--- scripts/update.php
+++ scripts/update.php
@@ -4,28 +4,48 @@ ini_set('display_errors', true);
 //Initialize all settings and autoloaders
 require_once(__DIR__ . "/../init.php");
 
-//1.  install all the default stuff.
+function preformPluginUpdate(\SiteMaster\Plugin\PluginInterface $plugin)
+{
+    static $checked;
 
+    if (!isset($checked)) {
+        $checked = array();
+    }
+
+    $name = $plugin->getMachineName();
+
+    //Have we already checked this plugin?
+    if (in_array($name, $checked)) {
+        return true;
+    }
+
+    $checked[] = $name;
+
+    if ($method = $plugin->getUpdateMethod()) {
+        echo 'Preforming ' . $method . ' on internal plugin: ' . $name . PHP_EOL;
+        return $plugin->preformUpdate();
+    }
+
+    return true;
+}
+
+//1.  install all the default stuff.
 $pluginManager = \SiteMaster\Plugin\PluginManager::getManager();
 
 //2.  trigger install on internal plugins
 $internalPlugins = $pluginManager->getInternalPlugins();
 foreach ($internalPlugins as $name=>$options) {
-    $plugin = $pluginManager->getPluginInfo($name);
-    if ($method = $plugin->getUpdateMethod()) {
-        echo 'Preforming ' . $method . ' on internal plugin: ' . $name . PHP_EOL;
-        $result = $plugin->preformUpdate();
-    }
+    preformPluginUpdate($pluginManager->getPluginInfo($name));
 }
 
 //2.  trigger install on external plugins
 $externalPlugins = $pluginManager->getExternalPlugins();
 foreach ($externalPlugins as $name=>$options) {
-    $plugin = $pluginManager->getPluginInfo($name);
-    if ($method = $plugin->getUpdateMethod()) {
-        echo 'Preforming ' . $method . ' on external plugin: ' . $name . PHP_EOL;
-        $result = $plugin->preformUpdate();
-    }
+    preformPluginUpdate($pluginManager->getPluginInfo($name));
 }
 
-//3. TODO: check for plugins that need to be uninstalled.
+//3. check for plugins that need to be uninstalled.
+$installedPlugins = $pluginManager->getInstalledPlugins();
+foreach ($installedPlugins as $name=>$plugin) {
+    preformPluginUpdate($plugin);
+}
