03cdf8341ec647d7fcb79f606486ea01e7c441c5
mfairchild365 <mfairchild365@gmail.com>
2013-11-29 17:48:51 -0600

    Add an install script

---
+++ scripts/install.php
@@ -0,0 +1,17 @@
+<?php
+echo '===== Installing... =====' . PHP_EOL;
+
+echo '===== Creating composer.json and installing base composer libraries =====' . PHP_EOL;
+echo 'This could take awhile...' . PHP_EOL;
+$root = dirname(__DIR__);
+$json = file_get_contents($root . '/base_composer.json');
+file_put_contents($root . '/composer.json', $json);
+
+//Update composer
+echo shell_exec('php ' . $root. '/composer.phar update');
+
+//Run Update Script
+echo '===== Running scripts/update.php to install database and plugins =====' . PHP_EOL;
+
+require_once($root. '/scripts/update.php');
+
