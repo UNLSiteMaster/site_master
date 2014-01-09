485ee49cb19b9f94504c5f2381a001357e5bd322
Michael Fairchild <mfairchild365@gmail.com>
2014-01-09 08:45:14 -0600

    Opening brace for class functions should be on the next line

--- src/SiteMaster/ViewableException.php
+++ src/SiteMaster/ViewableException.php
@@ -3,7 +3,8 @@ namespace SiteMaster;
 
 class ViewableException extends \Exception implements ViewableInterface
 {
-    public function __construct($message = "", $code = 0, $previous = NULL) {
+    public function __construct($message = "", $code = 0, $previous = NULL)
+    {
         parent::__construct($message, $code, $previous);
     }
 
