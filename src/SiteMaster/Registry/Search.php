5eb25904cc2887708723c3bc0f7f3e05a3b6364e
Michael Fairchild <mfairchild365@gmail.com>
2014-01-10 13:16:12 -0600

    Return a query result

--- src/SiteMaster/Registry/Search.php
+++ src/SiteMaster/Registry/Search.php
@@ -6,18 +6,30 @@ use \SiteMaster\ViewableInterface;
 
 class Search implements ViewableInterface
 {
+    public $sites = array();
+    public $result;
+    
     function __construct($options = array())
     {
+        if (isset($options['query'])) {
+            $this->result = $this->handleQuery($options['query']);
+        }
+    }
 
+    public function getPageTitle()
+    {
+        return "Registry";
     }
 
     public function getURL()
     {
         return Config::get('URL') . 'registry/';
     }
-
-    public function getPageTitle()
+    
+    protected function handleQuery($query_string)
     {
-        return "Registry";
+        $query = new Query();
+        
+        return $query->query($query_string);
     }
 }
