d69a18099c7b7eb53938665e07267a9cb78c7989
Michael Fairchild <mfairchild365@gmail.com>
2014-01-10 15:50:36 -0600

    Move foundation specific html to the foundation theme

--- www/templates/html/SiteMaster/Registry/Search.tpl.php
+++ www/templates/html/SiteMaster/Registry/Search.tpl.php
@@ -5,27 +5,20 @@ foreach ($authPlugins as $plugin) {
     $providers[] = $plugin->getProviderMachineName();
 }
 ?>
+<form action="<?php echo $context->getURL();?>" method="GET">
+    <label for="query">Enter Query</label>
+    <input type="text" id="query" name="query" placeholder="http://wwww.domain.com/" />
+    Examples:
+    <ul>
+        <li>Site: absolute URI, must include protocol (http://wwww.domain.com/)</li>
+        <li>Person: provider:uid (google:1111) 
+            <p>Available providers are: <?php echo implode(', ', $providers); ?></p>
+        </li>
+    </ul>
 
-<div class="panel">
-    <form action="<?php echo $context->getURL();?>" method="GET">
-        <label for="query">Enter Query</label>
-        <input type="text" id="query" name="query" placeholder="http://wwww.domain.com/" />
-        <div class="row">
-            <div class="small-6 columns">
-                Examples:
-                <ul>
-                    <li>Site: absolute URI, must include protocol (http://wwww.domain.com/)</li>
-                    <li>Person: provider:uid (google:1111) 
-                        <p>Available providers are: <?php echo implode(', ', $providers); ?></p>
-                    </li>
-                </ul>
-            </div>
-            <div class="small-6 columns">
-                <button type="submit" class="pull-right right">Query</button>
-            </div>
-        </div>
-    </form>
-</div>
+    <button type="submit">Query</button>
+    </div>
+</form>
 
 <?php
 if ($context->result) {
--- www/templates/html/SiteMaster/Registry/Site/Summary.tpl.php
+++ www/templates/html/SiteMaster/Registry/Site/Summary.tpl.php
@@ -1,6 +1,4 @@
-<div class="large-12 columns panel">
-    <a href="<?php echo $context->base_url ?>"><?php echo $context->getTitle(); ?></a>
-    <?php
-    echo $savvy->render($context->getMembers(), 'SiteMaster/Registry/Site/Members/Summary.tpl.php');
-    ?>
-</div>
+<a href="<?php echo $context->base_url ?>"><?php echo $context->getTitle(); ?></a>
+<?php
+echo $savvy->render($context->getMembers(), 'SiteMaster/Registry/Site/Members/Summary.tpl.php');
+?>
