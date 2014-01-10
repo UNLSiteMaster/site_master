7470040efd42c32401047982985bdb8abd11558a
Michael Fairchild <mfairchild365@gmail.com>
2014-01-10 13:24:41 -0600

    Display available providers

--- www/templates/html/SiteMaster/Registry/Search.tpl.php
+++ www/templates/html/SiteMaster/Registry/Search.tpl.php
@@ -1,3 +1,11 @@
+<?php
+$authPlugins = \Sitemaster\Plugin\PluginManager::getManager()->getAuthPlugins();
+$providers = array();
+foreach ($authPlugins as $plugin) {
+    $providers[] = $plugin->getProviderMachineName();
+}
+?>
+
 <div class="panel">
     <form action="<?php echo $context->getURL();?>" method="GET">
         <label for="query">Enter Query</label>
@@ -7,7 +15,9 @@
                 Examples:
                 <ul>
                     <li>Site: absolute URI, must include protocol (http://wwww.domain.com/)</li>
-                    <li>Person: provider:uid (google:1111)</li>
+                    <li>Person: provider:uid (google:1111) 
+                        <p>Available providers are: <?php echo implode(', ', $providers); ?></p>
+                    </li>
                 </ul>
             </div>
             <div class="small-6 columns">
