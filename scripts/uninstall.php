ae75f7fabfbc02ac5d7956e1a63fbfcd1d85cd19
Michael Fairchild <mfairchild365@gmail.com>
2014-01-13 16:57:29 -0600

    Add an uninstall script

---
+++ scripts/uninstall.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('display_errors', true);
+
+//Initialize all settings and autoloaders
+require_once(__DIR__ . "/../init.php");
+
+$pluginManager = \SiteMaster\Plugin\PluginManager::getManager();
+
+//3. check for plugins that need to be uninstalled.
+$installedPlugins = $pluginManager->getInstalledPlugins();
+foreach ($installedPlugins as $name=>$plugin) {
+    $plugin->uninstall();
+}
