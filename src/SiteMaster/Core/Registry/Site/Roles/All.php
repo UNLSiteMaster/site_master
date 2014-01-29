2c53aee8745bd6f208b7f583f4607ea424b4c021
Michael Fairchild <mfairchild365@gmail.com>
2014-01-29 13:51:27 -0600

    add a role listing

---
+++ src/SiteMaster/Core/Registry/Site/Roles/All.php
@@ -0,0 +1,36 @@
+<?php
+namespace SiteMaster\Core\Registry\Site\Roles;
+
+use DB\RecordList;
+
+class All extends RecordList
+{
+    public function __construct(array $options = array())
+    {
+        $options['array'] = self::getBySQL(array(
+            'sql'         => $this->getSQL(),
+            'returnArray' => true
+        ));
+
+        parent::__construct($options);
+    }
+
+    public function getDefaultOptions()
+    {
+        $options = array();
+        $options['itemClass'] = '\SiteMaster\Core\Registry\Site\Role';
+        $options['listClass'] = __CLASS__;
+
+        return $options;
+    }
+
+    public function getSQL()
+    {
+        //Build the list
+        $sql = "SELECT id
+                FROM roles
+                ORDER by role_name ASC";
+
+        return $sql;
+    }
+}
