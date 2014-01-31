707e33419a8e4bc8decae22a306807745fc411b9
Michael Fairchild <mfairchild365@gmail.com>
2014-01-31 13:52:16 -0600

    make the sites appear as a 'badge'

--- www/templates/html/SiteMaster/Core/Registry/Sites/All.tpl.php
+++ www/templates/html/SiteMaster/Core/Registry/Sites/All.tpl.php
@@ -5,11 +5,30 @@ if (!$context->count()) {
 <?php
 } else {
     ?>
-    <ul>
+    <ul class="site-list">
         <?php
         foreach ($context as $site) {
             ?>
-            <li><?php echo $site->getTitle() ?></li>
+            <li class="site gpa-4 clear-fix">
+                <a href="<?php echo $site->getURL(); ?>">
+                    <div class="general-details">
+                        <div class="url">
+                            <?php echo $site->base_url ?>
+                        </div>
+                        <div class="title">
+                            <?php echo $site->getTitle() ?>
+                        </div>
+                    </div>
+                    <div class="gpa">
+                        <div class="value">
+                            4.0
+                        </div>
+                        <div class="metric">
+                            GPA
+                        </div>
+                    </div>
+                </a>
+            </li>
         <?php
         }
         ?>
