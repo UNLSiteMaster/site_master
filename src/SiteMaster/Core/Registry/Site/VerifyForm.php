b95fcd4f5350cfc1643ec3c21366028a72811d78
Michael Fairchild <mfairchild365@gmail.com>
2014-02-03 10:15:14 -0600

    Simplify the constructor by moving logic into methods

--- src/SiteMaster/Core/Registry/Site/VerifyForm.php
+++ src/SiteMaster/Core/Registry/Site/VerifyForm.php
@@ -58,36 +58,9 @@ class VerifyForm implements ViewableInterface, PostHandlerInterface
         //Require login
         Session::requireLogin();
 
-        //get the site
-        if (!isset($this->options['site_id'])) {
-            throw new InvalidArgumentException('a site id is required', 400);
-        }
-
-        if (!$this->site = Site::getByID($this->options['site_id'])) {
-            throw new InvalidArgumentException('Could not find that site', 400);
-        }
-
-        $this->current_user = Session::getCurrentUser();
-        $this->current_user_membership = Member::getByUserIDAndSiteID($this->current_user->id, $this->site->id);
-
-        //Set the verify_user
-        if (isset($this->options['users_id']) && !empty($this->options['users_id'])) {
-            if (!$this->verify_user = User::getByID($this->options['users_id'])) {
-                throw new InvalidArgumentException('Could not find that user', 400);
-            }
-            $this->verify_membership = Member::getByUserIDAndSiteID($this->verify_user->id, $this->site->id);
-        } else {
-            $this->verify_user = $this->current_user;
-            $this->verify_user_membership = $this->current_user_membership;
-        }
-
-        if (!$this->verify_membership = Member::getByUserIDAndSiteID($this->verify_user->id, $this->site->id)) {
-            throw new InvalidArgumentException('Could not find a membership to verify', 400);
-        }
+        $this->setCurrentUser();
         
-        if ($this->verify_membership->isVerified()) {
-            throw new InvalidArgumentException('That membership is already verified', 400);
-        }
+        $this->setVerifyUser();
 
         if (!$this->canEdit()) {
             throw new AccessDeniedException('You do not have permission to join this user', 403);
@@ -117,6 +90,57 @@ class VerifyForm implements ViewableInterface, PostHandlerInterface
     public function getPageTitle()
     {
         return 'Verify Membership for ' . $this->verify_user->getName();
+    }
+
+    /**
+     * Set the current site
+     * 
+     * @throws \SiteMaster\Core\InvalidArgumentException
+     */
+    protected function setSite() {
+        if (!isset($this->options['site_id'])) {
+            throw new InvalidArgumentException('a site id is required', 400);
+        }
+
+        if (!$this->site = Site::getByID($this->options['site_id'])) {
+            throw new InvalidArgumentException('Could not find that site', 400);
+        }
+    }
+
+    /**
+     * Set the current user
+     */
+    protected function setCurrentUser()
+    {
+        $this->current_user = Session::getCurrentUser();
+        $this->current_user_membership = Member::getByUserIDAndSiteID($this->current_user->id, $this->site->id);
+    }
+
+    /**
+     * Set the verify User
+     * 
+     * @throws \SiteMaster\Core\InvalidArgumentException
+     */
+    protected function setVerifyUser()
+    {
+        //Set the verify_user
+        if (isset($this->options['users_id']) && !empty($this->options['users_id'])) {
+            if (!$this->verify_user = User::getByID($this->options['users_id'])) {
+                throw new InvalidArgumentException('Could not find that user', 400);
+            }
+            $this->verify_user_membership = Member::getByUserIDAndSiteID($this->verify_user->id, $this->site->id);
+        } else {
+            $this->verify_user = $this->current_user;
+            $this->verify_user_membership = $this->current_user_membership;
+        }
+
+        if (!$this->verify_user_membership = Member::getByUserIDAndSiteID($this->verify_user->id, $this->site->id)) {
+            throw new InvalidArgumentException('Could not find a membership to verify', 400);
+        }
+
+        if ($this->verify_user_membership->isVerified()) {
+            throw new InvalidArgumentException('That membership is already verified', 400);
+        }
     }
 
     /**
@@ -213,7 +237,7 @@ class VerifyForm implements ViewableInterface, PostHandlerInterface
             throw new RuntimeException('Unable to find the verification file.  Please make sure it is present and try again.', 400);
         }
         
-        $this->verify_membership->verify();
+        $this->verify_user_membership->verify();
     }
 
     protected function bypassVerify()
@@ -222,7 +246,7 @@ class VerifyForm implements ViewableInterface, PostHandlerInterface
             throw new AccessDeniedException('You do not have permission to bypass verification');
         }
 
-        $this->verify_membership->verify();
+        $this->verify_user_membership->verify();
     }
 
     public function getEditURL()
@@ -232,6 +256,6 @@ class VerifyForm implements ViewableInterface, PostHandlerInterface
 
     public function getVerificationURL()
     {
-        return $this->site->base_url . 'sitemaster_v_' . $this->verify_membership->verification_code . '.txt';
+        return $this->site->base_url . 'sitemaster_v_' . $this->verify_user_membership->verification_code . '.txt';
     }
 }
