0ee4e984c26a44e54ae06a4ea83ca798ef81379b
Michael Fairchild <mfairchild365@gmail.com>
2014-02-07 15:29:04 -0600

    Only Log, don't schedule

--- src/SiteMaster/Core/Scan/Logger/Basic.php
+++ src/SiteMaster/Core/Scan/Logger/Basic.php
@@ -19,23 +19,6 @@ class Basic extends \Spider_LoggerAbstract
     
     public function log($uri, $depth, DOMXPath $xpath)
     {
-        //Util::log(Logger::INFO, 'Logged: ' . $uri);
-        $pages = $this->spider->getCrawlableUris($this->spider->getStartBase(), \Spider::getURIBase($uri), $uri, $xpath);
-        
-        foreach ($pages as $uri) {
-            $pheanstalk = new \Pheanstalk_Pheanstalk('0.0.0.0');
-            
-            $data = array(
-                'controller'=>'scan-page',
-                'data' => array(
-                    'uri'=>$uri,
-                    'scanned_page_id'=>0,
-                    'crawl'=>1,
-                    'update'=>false
-                )
-            );
-
-            $task_id = $pheanstalk->useTube('testtube')->put(json_encode($data));
-        }
+        Util::log(Logger::INFO, 'Logged: ' . $uri);
     }
 }
