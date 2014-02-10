caedda1fa58037b0891eef552f9506f2acd832e6
Michael Fairchild <mfairchild365@gmail.com>
2014-02-10 10:33:37 -0600

    Add a queued list

---
+++ src/SiteMaster/Core/Scan/Site/Pages/Queued.php
@@ -0,0 +1,43 @@
+<?php
+namespace SiteMaster\Core\Scan\Site\Pages;
+
+use DB\RecordList;
+
+class Queued extends RecordList
+{
+    public function __construct(array $options = array())
+    {
+        $options['array'] = self::getBySQL(array(
+            'sql'         => $this->getSQL(),
+            'returnArray' => true
+        ));
+
+        parent::__construct($options);
+    }
+
+    public function getDefaultOptions()
+    {
+        $options = array();
+        $options['itemClass'] = '\SiteMaster\Core\Scan\Site\Page';
+        $options['listClass'] = __CLASS__;
+
+        return $options;
+    }
+    
+    public function getLimit()
+    {
+        return 1;
+    }
+
+    public function getSQL()
+    {
+        //Build the list
+        $sql = "SELECT scanned_page.id
+                FROM scanned_page
+                WHERE status = 'QUEUED'
+                ORDER BY priority ASC, start_time ASC
+                LIMIT " . (int)$this->getLimit();
+
+        return $sql;
+    }
+}
