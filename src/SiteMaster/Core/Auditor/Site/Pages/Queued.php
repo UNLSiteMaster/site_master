33619393aebc740fdea8a294fa4d3517b73ae711
Michael Fairchild <mfairchild365@gmail.com>
2014-02-10 11:13:05 -0600

    Allow filtering by the scans_id

--- src/SiteMaster/Core/Auditor/Site/Pages/Queued.php
+++ src/SiteMaster/Core/Auditor/Site/Pages/Queued.php
@@ -7,6 +7,8 @@ class Queued extends RecordList
 {
     public function __construct(array $options = array())
     {
+        $this->options = $options + $this->options;
+        
         $options['array'] = self::getBySQL(array(
             'sql'         => $this->getSQL(),
             'returnArray' => true
@@ -24,6 +26,17 @@ class Queued extends RecordList
         return $options;
     }
     
+    public function getWhere()
+    {
+        $sql = "WHERE status = 'QUEUED'";
+        
+        if (isset($this->options['scans_id'])) {
+            $sql .= " AND scans_id = " . (int)$this->options['scans_id'];
+        }
+        
+        return $sql;
+    }
+    
     public function getLimit()
     {
         return 1;
@@ -34,7 +47,7 @@ class Queued extends RecordList
         //Build the list
         $sql = "SELECT scanned_page.id
                 FROM scanned_page
-                WHERE status = 'QUEUED'
+                " . $this->getWhere() . "
                 ORDER BY priority ASC, start_time ASC
                 LIMIT " . (int)$this->getLimit();
 
