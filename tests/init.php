d182974809c303fb21985bc3e477188d3eab267f
Michael Fairchild <mfairchild365@gmail.com>
2014-02-12 14:17:42 -0600

    Only test with a with a defined set of plugins by default

--- tests/init.php
+++ tests/init.php
@@ -10,9 +10,16 @@ if (file_exists(__DIR__ . '/../config.inc.php')) {
 
 require_once $config_file;
 
+//Prevent tests from running on the production db
 Config::set('DB_HOST'     , '');
 Config::set('DB_USER'     , '');
 Config::set('DB_PASSWORD' , '');
 Config::set('DB_NAME'     , '');
+
+//Only test with the default plugins
+Config::set('PLUGINS', array(
+    'example' => array('setting'=>'value'),
+    'theme_foundation' => array('setting'=>'value'),
+));
 
 require_once(__DIR__ . '/../init_plugins.php');
