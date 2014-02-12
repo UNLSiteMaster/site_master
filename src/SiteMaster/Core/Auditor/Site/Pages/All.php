f04585fa5ce7bce38d2dc57da14908774c1c94d1
Michael Fairchild <mfairchild365@gmail.com>
2014-02-12 14:41:32 -0600

    Add more lists for pages

---
+++ src/SiteMaster/Core/Auditor/Site/Pages/All.php
@@ -0,0 +1,68 @@
+<?php
+namespace SiteMaster\Core\Auditor\Site\Pages;
+
+use DB\RecordList;
+
+class All extends RecordList
+{
+    public function __construct(array $options = array())
+    {
+        $this->options = $options + $this->options;
+
+        $options['array'] = self::getBySQL(array(
+            'sql'         => $this->getSQL(),
+            'returnArray' => true
+        ));
+
+        parent::__construct($options);
+    }
+
+    public function getDefaultOptions()
+    {
+        $options = array();
+        $options['itemClass'] = '\SiteMaster\Core\Auditor\Site\Page';
+        $options['listClass'] = __CLASS__;
+
+        return $options;
+    }
+
+    public function getWhere()
+    {
+        $sql = "WHERE status = 'QUEUED'";
+
+        if (isset($this->options['scans_id'])) {
+            $sql .= " AND scans_id = " . (int)$this->options['scans_id'];
+        }
+
+        return $sql;
+    }
+
+    public function getLimit()
+    {
+        return '';
+    }
+    
+    public function getOrderBy()
+    {
+        return 'ORDER BY uri ASC';
+    }
+
+    public function getSQL()
+    {
+        //Build the list
+        $sql = "SELECT scanned_page.id
+                FROM scanned_page
+                " . $this->getWhere() . "
+                " . $this->getOrderBy() . "
+                " . $this->getLimit();
+
+        return $sql;
+    }
+
+    /**
+     * @return \SiteMaster\Core\Auditor\Site\Page
+     */
+    function current() {
+        return parent::current();
+    }
+}
--- src/SiteMaster/Core/Auditor/Site/Pages/Queued.php
+++ src/SiteMaster/Core/Auditor/Site/Pages/Queued.php
@@ -1,56 +1,15 @@
 <?php
 namespace SiteMaster\Core\Auditor\Site\Pages;
 
-use DB\RecordList;
-
-class Queued extends RecordList
+class Queued extends All
 {
-    public function __construct(array $options = array())
-    {
-        $this->options = $options + $this->options;
-        
-        $options['array'] = self::getBySQL(array(
-            'sql'         => $this->getSQL(),
-            'returnArray' => true
-        ));
-
-        parent::__construct($options);
-    }
-
-    public function getDefaultOptions()
-    {
-        $options = array();
-        $options['itemClass'] = '\SiteMaster\Core\Auditor\Site\Page';
-        $options['listClass'] = __CLASS__;
-
-        return $options;
-    }
-    
-    public function getWhere()
-    {
-        $sql = "WHERE status = 'QUEUED'";
-        
-        if (isset($this->options['scans_id'])) {
-            $sql .= " AND scans_id = " . (int)$this->options['scans_id'];
-        }
-        
-        return $sql;
-    }
-    
     public function getLimit()
     {
-        return 1;
+        return 'LIMIT 1';
     }
 
-    public function getSQL()
+    public function getOrderBy()
     {
-        //Build the list
-        $sql = "SELECT scanned_page.id
-                FROM scanned_page
-                " . $this->getWhere() . "
-                ORDER BY priority ASC, start_time ASC
-                LIMIT " . (int)$this->getLimit();
-
-        return $sql;
+        return 'ORDER BY priority ASC, start_time ASC';
     }
 }
