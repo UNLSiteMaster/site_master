ea5ff7bea765efbb9b8eefd2e7c149672a097fd6
Michael Fairchild <mfairchild365@gmail.com>
2014-02-20 14:22:55 -0600

    Prevent unintended updates to libraries
    This no longer runs `composer update` on plugins.  Nor do we generate a master composer.json file anymore.  Instead, let plugins commit a composer.json and composer.lock file, and then run `composer install` on them, so that we can ensure that the same libraries are installed every time.

--- scripts/install.php
+++ scripts/install.php
@@ -4,11 +4,9 @@ echo '===== Installing... =====' . PHP_EOL;
 echo '===== Creating composer.json and installing base composer libraries =====' . PHP_EOL;
 echo 'This could take awhile...' . PHP_EOL;
 $root = dirname(__DIR__);
-$json = file_get_contents($root . '/base_composer.json');
-file_put_contents($root . '/composer.json', $json);
 
 //Update composer
-passthru('php ' . $root. '/composer.phar update');
+passthru('php ' . $root. '/composer.phar install');
 
 require_once($root. '/scripts/update_libs.php');
 
