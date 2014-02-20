6ca2be2dd63ab70bdde5fd57c9ef902fa130e5c9
Michael Fairchild <mfairchild365@gmail.com>
2014-02-20 14:08:09 -0600

    use a whitelist

--- plugins/metric_links/src/Filters/Scheme.php
+++ plugins/metric_links/src/Filters/Scheme.php
@@ -6,12 +6,11 @@ class Scheme extends \Spider_UriFilterInterface
     function accept()
     {
         //$this->current() contains the base uri, so the 'protocol' it probably won't start with the following.
-        if (stripos($this->current(), 'javascript:') !== false
-            || stripos($this->current(), 'tel:') !== false
-            || stripos($this->current(), 'mailto:') !== false) {
-            return false;
+        if (stripos($this->current(), 'http:') === 0
+            || stripos($this->current(), 'https:') === 0) {
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
