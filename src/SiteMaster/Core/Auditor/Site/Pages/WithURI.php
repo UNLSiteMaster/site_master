fcc2036d428604fd8a0833c90f3163273c93abe7
Michael Fairchild <mfairchild365@gmail.com>
2014-02-21 08:47:55 -0600

    Add a record list for all pages with a URI

---
+++ src/SiteMaster/Core/Auditor/Site/Pages/WithURI.php
@@ -0,0 +1,42 @@
+<?php
+namespace SiteMaster\Core\Auditor\Site\Pages;
+
+use SiteMaster\Core\InvalidArgumentException;
+
+class WithURI extends All
+{
+    public function __construct(array $options = array())
+    {
+        if (!isset($options['uri'])) {
+            throw new InvalidArgumentException('the uri must be set', 500);
+        }
+
+        parent::__construct($options);
+    }
+
+    public function getWhere()
+    {
+        $where = "WHERE  uri = '" . self::escapeString($this->options['uri']) . "'";
+
+        if (isset($this->options['not_id'])) {
+            $where .= " AND scanned_page.id != " . (int)$this->options['not_id'];
+        }
+
+        return $where;
+    }
+
+    public function getLimit()
+    {
+        if (isset($this->options['limit'])) {
+            return 'LIMIT ' . (int)$this->options['limit'];
+        }
+
+        return '';
+    }
+
+    public function getOrderBy()
+    {
+        //We want the newest first
+        return 'ORDER BY date_created DESC';
+    }
+}
