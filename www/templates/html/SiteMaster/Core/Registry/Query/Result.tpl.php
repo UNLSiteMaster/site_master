d29aa943a96700140d24bec87df243d527051737
Michael Fairchild <mfairchild365@gmail.com>
2014-02-24 14:13:03 -0600

    Display a 'sorry, no results found' message

--- www/templates/html/SiteMaster/Core/Registry/Query/Result.tpl.php
+++ www/templates/html/SiteMaster/Core/Registry/Query/Result.tpl.php
@@ -1,4 +1,10 @@
 <?php
-foreach ($context as $site) {
-    echo $savvy->render($site, 'SiteMaster/Core/Registry/Site/Summary.tpl.php');
+if ($context->getInnerIterator()->count() == 0) {
+    ?>
+    Sorry, no sites could be found
+    <?php
+} else {
+    foreach ($context as $site) {
+        echo $savvy->render($site, 'SiteMaster/Core/Registry/Site/Summary.tpl.php');
+    }
 }
