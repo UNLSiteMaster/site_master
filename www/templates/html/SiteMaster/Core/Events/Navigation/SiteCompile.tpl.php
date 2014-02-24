fc321cd4504d60ae7378c2449ec9dc67097c77f4
Michael Fairchild <mfairchild365@gmail.com>
2014-02-24 13:44:51 -0600

    Display an external link

--- www/templates/html/SiteMaster/Core/Events/Navigation/SiteCompile.tpl.php
+++ www/templates/html/SiteMaster/Core/Events/Navigation/SiteCompile.tpl.php
@@ -7,14 +7,15 @@ $site = $context->getSite();
 
 ?>
 <div class="site-nav">
-    <div class="url"><?php echo $site->base_url;?></div>
     <div class="title"><?php echo $site->getTitle();?></div>
+    <div class="url">
+        <?php echo $site->base_url;?>
+        <a href="<?php echo $site->base_url;?>" class="external" title="open the external page"><img src="<?php echo \SiteMaster\Core\Config::get('URL') ?>www/images/external.png" alt="link to external site"/></a>
+    </div>
     <div class="menu-button site-nav">Menu</div>
     <nav class="clear-fix">
         <ul data-breakpoint="800" class="flexnav">
             <?php
-            
-
             foreach ($nav as $url=>$title) {
                 ?>
                 <li>
