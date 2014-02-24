aecd9846b1a3a820542677607a1768a5373bfa3c
Michael Fairchild <mfairchild365@gmail.com>
2014-02-24 14:49:18 -0600

    Style the search results

--- www/templates/html/SiteMaster/Core/Registry/Query/Result.tpl.php
+++ www/templates/html/SiteMaster/Core/Registry/Query/Result.tpl.php
@@ -1,13 +1,17 @@
-<?php
-if ($context->getInnerIterator()->count() == 0) {
-    ?>
-    Sorry, no sites could be found
+<div class="results">
     <?php
-} else {
-    ?>
-    <h2>Results</h2>
-    <?php
-    foreach ($context as $site) {
-        echo $savvy->render($site, 'SiteMaster/Core/Registry/Site/Summary.tpl.php');
+        if ($context->getInnerIterator()->count() == 0) {
+            ?>
+            Sorry, no sites could be found
+        <?php
+    } else {
+        ?>
+        <h2>Results</h2>
+        <?php
+        foreach ($context as $site) {
+            echo $savvy->render($site, 'SiteMaster/Core/Registry/Site/Summary.tpl.php');
+        }
     }
-}
+    ?>
+</div>
+
--- www/templates/html/SiteMaster/Core/Registry/Site/Summary.tpl.php
+++ www/templates/html/SiteMaster/Core/Registry/Site/Summary.tpl.php
@@ -1,6 +1,14 @@
 <div class="panel">
-<a href="<?php echo $context->base_url ?>"><?php echo $context->getTitle(); ?></a>
-<?php
-echo $savvy->render($context->getApprovedMembers(), 'SiteMaster/Core/Registry/Site/Members/Summary.tpl.php');
-?>
+    <div class="site-header">
+        <span class="site-title">
+            <a href="<?php echo $context->getURL() ?>"><?php echo $context->getTitle() ?></a>
+        </span>
+        <span class="site-url">
+            <?php echo $context->base_url ?>
+            <a href="<?php echo $context->base_url;?>" class="external" title="open the external page"><img src="<?php echo \SiteMaster\Core\Config::get('URL') ?>www/images/external.png" alt="link to external site"/></a>
+        </span>
+    </div>
+    <?php
+    echo $savvy->render($context->getApprovedMembers(), 'SiteMaster/Core/Registry/Site/Members/Summary.tpl.php');
+    ?>
 </div>
