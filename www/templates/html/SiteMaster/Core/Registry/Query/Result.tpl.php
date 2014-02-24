9e3e07bf6965ff44fd41aafb0ecaa3bd1e34ffc4
Michael Fairchild <mfairchild365@gmail.com>
2014-02-24 14:38:16 -0600

    wrap results in panels

--- www/templates/html/SiteMaster/Core/Registry/Query/Result.tpl.php
+++ www/templates/html/SiteMaster/Core/Registry/Query/Result.tpl.php
@@ -4,6 +4,9 @@ if ($context->getInnerIterator()->count() == 0) {
     Sorry, no sites could be found
     <?php
 } else {
+    ?>
+    <h2>Results</h2>
+    <?php
     foreach ($context as $site) {
         echo $savvy->render($site, 'SiteMaster/Core/Registry/Site/Summary.tpl.php');
     }
--- www/templates/html/SiteMaster/Core/Registry/Site/Summary.tpl.php
+++ www/templates/html/SiteMaster/Core/Registry/Site/Summary.tpl.php
@@ -1,4 +1,6 @@
+<div class="panel">
 <a href="<?php echo $context->base_url ?>"><?php echo $context->getTitle(); ?></a>
 <?php
 echo $savvy->render($context->getApprovedMembers(), 'SiteMaster/Core/Registry/Site/Members/Summary.tpl.php');
 ?>
+</div>
