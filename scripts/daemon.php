a4234771fe0cbc8eb86cbae2e46939e77cb54f34
Michael Fairchild <mfairchild365@gmail.com>
2014-02-25 15:13:18 -0600

    Clear filemtime cache

--- scripts/daemon.php
+++ scripts/daemon.php
@@ -8,6 +8,7 @@ $config_file = __DIR__ . '/../config.inc.php';
 $last_modified = filemtime($config_file);
 
 while (true) {
+    clearstatcache(false, $config_file);
     //Check the last modified time to see if we need to load a new config
     if (filemtime($config_file) > $last_modified) {
         SiteMaster\Core\Util::log(Monolog\Logger::NOTICE, 'stopping daemon due to a change in config.inc.php');
