967685300cafccee668aeac58645eb3b9bed202c
Michael Fairchild <mfairchild365@gmail.com>
2014-02-25 15:47:10 -0600

    Reschedule running pages on daemon start
    There should only ever be one worker.  In the event that a job did not finish, destroy it and try it again.

--- scripts/daemon.php
+++ scripts/daemon.php
@@ -7,6 +7,16 @@ require_once(__DIR__ . "/../init.php");
 $config_file = __DIR__ . '/../config.inc.php';
 $last_modified = filemtime($config_file);
 
+
+//There should only be one worker running at a time.  If there are running jobs, remove them and try again...
+$running = new SiteMaster\Core\Auditor\Site\Pages\Running();
+foreach ($running as $page) {
+    //Log it
+    SiteMaster\Core\Util::log(Monolog\Logger::ALERT, 'Re-scheduling running job: pages.id=' . $page->id);
+    
+    $page->rescheduleScan();
+}
+
 while (true) {
     clearstatcache(false, $config_file);
     //Check the last modified time to see if we need to load a new config
