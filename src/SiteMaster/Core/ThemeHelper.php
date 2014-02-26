b2e21f1883cf3bbe4c9a0fe3e05cbbf54fbd6e70
Michael Fairchild <mfairchild365@gmail.com>
2014-02-26 09:50:51 -0600

    Docs

--- src/SiteMaster/Core/ThemeHelper.php
+++ src/SiteMaster/Core/ThemeHelper.php
@@ -2,7 +2,14 @@
 namespace SiteMaster\Core;
 
 class ThemeHelper {
-    
+
+    /**
+     * Trim the base url off of a given url
+     * 
+     * @param string $base_url
+     * @param string $url
+     * @return mixed
+     */
     public function trimBaseURL($base_url, $url)
     {
         return str_replace(strtolower($base_url), '', strtolower($url));
