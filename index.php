b6e8964fe23d0d6c05f45b1787baf8dc2ebbd461
Michael Fairchild <mfairchild365@gmail.com>
2014-02-26 09:53:26 -0600

    correct the name

--- index.php
+++ index.php
@@ -19,6 +19,6 @@ $savvy->addGlobal('plugin_manager', \SiteMaster\Core\Plugin\PluginManager::getMa
 $savvy->addGlobal('user', \SiteMaster\Core\User\Session::getCurrentUser());
 $savvy->addGlobal('grading_helper', new \SiteMaster\Core\Auditor\GradingHelper());
 $savvy->addGlobal('base_url', \SiteMaster\Core\Config::get('BASE_URL'));
-$savvy->addGlobal('template_helper', new \SiteMaster\Core\ThemeHelper());
+$savvy->addGlobal('theme_helper', new \SiteMaster\Core\ThemeHelper());
 
 echo $savvy->render($app);
