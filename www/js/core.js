3930296cf86e723266222f1d55f3f57016f0a53b
Michael Fairchild <mfairchild365@gmail.com>
2014-02-28 08:26:04 -0600

    missing id selector

--- www/js/core.js
+++ www/js/core.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(".flexnav").flexNav();
 
-    $("delete-site").click(function(e) {
+    $("#delete-site").click(function(e) {
         return confirm('Are you sure you want to delete this site?');
     });
     
