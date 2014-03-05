66699cda2456a3312bb735182c9e2d1d868e4437
Michael Fairchild <mfairchild365@gmail.com>
2014-03-05 09:38:55 -0600

    Confirm the scan

--- www/js/core.js
+++ www/js/core.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
     $("#delete-site").click(function(e) {
         return confirm('Are you sure you want to delete this site?');
     });
+
+    $(".scan-site").click(function(e) {
+        return confirm('Scanning a site may take a long time, do you wish to continue?');
+    });
     
     $(".scan table, .metric-grade-details table").tablesorter();
     
-    $(".close-action").click(function() {     
+    $(".close-action").click(function() {
         window.location.hash = '!';
         return false;
     });
 });
-
-
