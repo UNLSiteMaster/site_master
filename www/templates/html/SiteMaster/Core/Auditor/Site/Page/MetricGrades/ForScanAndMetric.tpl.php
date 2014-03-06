21e671e89b81de62ddca091a3c05268b3fb03395
Michael Fairchild <mfairchild365@gmail.com>
2014-03-06 11:05:59 -0600

    Don't display a table if there are no marks

--- www/templates/html/SiteMaster/Core/Auditor/Site/Page/MetricGrades/ForScanAndMetric.tpl.php
+++ www/templates/html/SiteMaster/Core/Auditor/Site/Page/MetricGrades/ForScanAndMetric.tpl.php
@@ -1,28 +1,38 @@
 <?php
 $scan = $context->getScan();
 $site = $scan->getSite();
-?>
 
-<table>
-    <thead>
-    <tr>
-        <th>Page</th>
-        <th>Grade</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($context as $grade) {
-        $page = $grade->getPage();
-        ?>
-        <tr>
-            <td>
-                <a href="<?php echo $page->getURL()?>"><?php echo $theme_helper->trimBaseURL($site->base_url, $page->uri) ?></a>
-            </td>
-            <td><?php echo $grade->letter_grade ?></td>
-        </tr>
-    <?php
-    }
+if ($context->count()) {
     ?>
-    </tbody>
-</table>
+    <table>
+        <thead>
+        <tr>
+            <th>Page</th>
+            <th>Grade</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($context as $grade) {
+            $page = $grade->getPage();
+            ?>
+            <tr>
+                <td>
+                    <a href="<?php echo $page->getURL()?>"><?php echo $theme_helper->trimBaseURL($site->base_url, $page->uri) ?></a>
+                </td>
+                <td><?php echo $grade->letter_grade ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+        </tbody>
+    </table>
+    <?php
+} else {
+    ?>
+    <p>Everything looks good!</p>
+    <?php
+}
+
+
+
