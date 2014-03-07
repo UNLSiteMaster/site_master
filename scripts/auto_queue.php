9ac729a6aa4bfd698200066ab952d6fa79a4688d
Michael Fairchild <mfairchild365@gmail.com>
2014-03-07 14:27:55 -0600

    Figure the number of sites to queue each day in order to scan them all once a month

--- scripts/auto_queue.php
+++ scripts/auto_queue.php
@@ -4,7 +4,12 @@ ini_set('display_errors', true);
 //Initialize all settings and autoloaders
 require_once(__DIR__ . "/../init.php");
 
-$queue = new \SiteMaster\Core\Registry\Sites\AutoQueue();
+$sites = new \SiteMaster\Core\Registry\Sites\All();
+
+//Figure the number of sites to queue each day in order to scan them all once a month (total sites/31 days) 
+$limit = round($sites->count() / 31);
+
+$queue = new \SiteMaster\Core\Registry\Sites\AutoQueue(array('queue_limit' => $limit));
 
 foreach ($queue as $site) {
     $site->scheduleScan();
