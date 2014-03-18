885208494d11912b2c9902299b0985af7e84fc2a
Michael Fairchild <mfairchild365@gmail.com>
2014-03-18 15:54:10 -0500

    Add the query to get a list of all users who are not operators

--- data/helpful_queries.sql
+++ data/helpful_queries.sql
@@ -7,4 +7,14 @@ FROM
     SELECT scans.gpa as gpa, max(scans.id) as id
     FROM scans
     GROUP BY scans.sites_id
-  ) t
+  ) t;
+  
+#Get all users with emails (who are not operators)
+select users.uid, users.first_name, users.last_name, users.email
+FROM users
+  LEFT JOIN site_members ON site_members.users_id = users.id
+  LEFT JOIN site_member_roles ON site_member_roles.site_members_id = site_members.id
+  LEFT JOIN roles ON site_member_roles.roles_id = roles.id
+WHERE roles.role_name NOT IN ('operator')
+      AND users.email IS NOT NULL
+GROUP BY users.uid;
