a6c30b9384e1f1c9f66780ed37086c3246d87769
Michael Fairchild <mfairchild365@gmail.com>
2014-03-26 11:28:36 -0500

    add some more helpful queries

--- data/helpful_queries.sql
+++ data/helpful_queries.sql
@@ -3,9 +3,9 @@
 SELECT AVG(gpa)
 FROM
   (
-
     SELECT scans.gpa as gpa, max(scans.id) as id
     FROM scans
+      WHERE status = 'COMPLETE'
     GROUP BY scans.sites_id
   ) t;
   
@@ -18,3 +18,30 @@ FROM users
 WHERE roles.role_name NOT IN ('operator')
       AND users.email IS NOT NULL
 GROUP BY users.uid;
+
+#number of sites in 4.0
+SELECT COUNT(*)
+FROM
+  (
+    SELECT unl_scan_attributes.id, max(scans.id)
+    FROM unl_scan_attributes
+      JOIN scans on (unl_scan_attributes.scans_id = scans.id)
+    WHERE scans.status = 'COMPLETE'
+          AND unl_scan_attributes.html_version = '4.0'
+          AND unl_scan_attributes.dep_version >= '4.0'
+    GROUP by scans.sites_id
+  ) as t;
+  
+#top marks
+SELECT COUNT(marks_id) as count, page_marks.marks_id, marks.name
+FROM page_marks
+  JOIN scanned_page ON (scanned_page.id = page_marks.scanned_page_id)
+  JOIN (SELECT MAX(scans.id) as id
+        FROM scanned_page
+          JOIN scans on (scanned_page.scans_id = scans.id)
+        WHERE scans.status = 'COMPLETE'
+        GROUP BY scans.sites_id
+       ) as completed_scans ON completed_scans.id = scanned_page.scans_id
+  JOIN marks ON (page_marks.marks_id = marks.id)
+GROUP BY page_marks.marks_id
+ORDER BY count DESC;
