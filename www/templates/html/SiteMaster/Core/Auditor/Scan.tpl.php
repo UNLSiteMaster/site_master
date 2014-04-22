9b76cd5dbfe0133540850b88f109e6726b5fb625
Michael Fairchild <mfairchild365@gmail.com>
2014-04-22 10:48:59 -0500

    Wrap in sections

--- www/templates/html/SiteMaster/Core/Auditor/Scan.tpl.php
+++ www/templates/html/SiteMaster/Core/Auditor/Scan.tpl.php
@@ -6,128 +6,130 @@ $site_pass_fail = $context->isPassFail();
 ?>
 
 <div class="scan">
-    <header>
-        <h2>
-            <?php
-            $date = date("n-j-y g:i a", strtotime($context->start_time));
-            if (!$context->isComplete()) {
-                $date = '--';
-            }
+    <section id="summary">
+        <header>
+            <h2>
+                <?php
+                $date = date("n-j-y g:i a", strtotime($context->start_time));
+                if (!$context->isComplete()) {
+                    $date = '--';
+                }
+                ?>
+                Scan: <?php echo $date ?>
+            </h2>
+            <div class="sub-info">
+                Status: <span class="scan-status"><?php echo $context->status;?></span>
+                <span class="scan-queue-position"></span>
+                <?php
+                if (!$context->isComplete()) {
+                    echo $savvy->render($context->getProgress());
+                }
+                ?>
+            </div>
+        </header>
+    
+        <?php
+        if (!$context->isComplete()) {
             ?>
-            Scan: <?php echo $date ?>
-        </h2>
-        <div class="sub-info">
-            Status: <span class="scan-status"><?php echo $context->status;?></span>
-            <span class="scan-queue-position"></span>
-            <?php
-            if (!$context->isComplete()) {
-                echo $savvy->render($context->getProgress());
-            }
-            ?>
-        </div>
-    </header>
-
-    <?php
-    if (!$context->isComplete()) {
-        ?>
-        <div class="panel notice">
-            <img src="<?php echo $base_url . 'www/images/loading.gif' ?>" />
-            This scan has not finished yet.  This page will automatically refresh when the scan is complete.
-        </div>
-    <?php
-    }
-    ?>
-
-    <?php
-    if ($site_pass_fail && $context->isComplete()) {
-        $passing = false;
-        if ($context->gpa == 100) {
-            $passing = true;
+            <div class="panel notice">
+                <img src="<?php echo $base_url . 'www/images/loading.gif' ?>" />
+                This scan has not finished yet.  This page will automatically refresh when the scan is complete.
+            </div>
+        <?php
         }
         ?>
-        <div class="dashboard-metrics">
-            <div class="visual-island site-pass-fail-status <?php echo ($passing)?'valid':'invalid'; ?>">
-                <span class="dashboard-value">
-                    <?php
-                    if ($passing) {
-                        echo 'Looks Good';
-                    } else {
-                        echo 'Needs Work';
-                    }
-                    ?>
-                </span>
-                <span class="dashboard-metric">
-                    <?php
-                    if ($passing) {
-                        echo 'All of your pages are passing.  Good job!';
-                    } else {
-                        echo 'In order for the site to pass, all pages must pass.';
-                    }
-                    ?>
-                </span>
-            </div>
-        </div>
-    <?php
-    }
-    ?>
     
-    <section class="row dashboard-metrics">
-        <div class="large-3 columns">
-            <div class="visual-island gpa">
-                <span class="dashboard-value">
-                     <?php
-                     if ($context->isComplete()) {
-                         echo $context->gpa . ($site_pass_fail?'%':'');
-                     } else {
-                         echo '--';
-                     }
-                     ?>
-                </span>
-                <?php
-                $gpa_name = 'GPA';
-                if ($site_pass_fail) {
-                    $gpa_name = 'of pages are passing';
-                }
-                ?>
-                <span class="dashboard-metric"><?php echo $gpa_name ?></span>
-            </div>
-        </div>
-        <div class="large-3 columns">
-            <div class="visual-island">
-                <?php
-                $arrow = "&#8596; <span class='secondary'>(same)</span>";
-                if ($previous_scan) {
-                    if ($previous_scan->gpa > $context->gpa) {
-                        $arrow = "&#8595; <span class='secondary'>(worse)</span>";
-                    } else if ($previous_scan->gpa < $context->gpa) {
-                        $arrow = "&#8593; <span class='secondary'>(better)</span>";
-                    }
-
-                    if ($site_pass_fail != $previous_scan->isPassFail()) {
-                        $arrow = "&#8800; <span class='secondary'>(incomparable)</span>";
-                    }
-                }
-                ?>
-                <div class="dashboard-value">
-                    <?php echo $arrow ?>
+        <?php
+        if ($site_pass_fail && $context->isComplete()) {
+            $passing = false;
+            if ($context->gpa == 100) {
+                $passing = true;
+            }
+            ?>
+            <div class="dashboard-metrics">
+                <div class="visual-island site-pass-fail-status <?php echo ($passing)?'valid':'invalid'; ?>">
+                    <span class="dashboard-value">
+                        <?php
+                        if ($passing) {
+                            echo 'Looks Good';
+                        } else {
+                            echo 'Needs Work';
+                        }
+                        ?>
+                    </span>
+                    <span class="dashboard-metric">
+                        <?php
+                        if ($passing) {
+                            echo 'All of your pages are passing.  Good job!';
+                        } else {
+                            echo 'In order for the site to pass, all pages must pass.';
+                        }
+                        ?>
+                    </span>
                 </div>
-                <div class="dashboard-metric">Compared to Last Scan</div>
             </div>
-        </div>
-        <div class="large-3 columns">
-            <div class="visual-island">
-                <span class="dashboard-value"><?php echo $context->getABSNumberOfChanges() ?></span>
-                <span class="dashboard-metric">Changes</span>
+        <?php
+        }
+        ?>
+    
+        <div class="row dashboard-metrics">
+            <div class="large-3 columns">
+                <div class="visual-island gpa">
+                    <span class="dashboard-value">
+                         <?php
+                         if ($context->isComplete()) {
+                             echo $context->gpa . ($site_pass_fail?'%':'');
+                         } else {
+                             echo '--';
+                         }
+                         ?>
+                    </span>
+                    <?php
+                    $gpa_name = 'GPA';
+                    if ($site_pass_fail) {
+                        $gpa_name = 'of pages are passing';
+                    }
+                    ?>
+                    <span class="dashboard-metric"><?php echo $gpa_name ?></span>
+                </div>
             </div>
-        </div>
-        <div class="large-3 columns">
-            <div class="visual-island">
-                <span class="dashboard-value"><?php echo $pages->count() ?></span>
-                <span class="dashboard-metric">Pages</span>
+            <div class="large-3 columns">
+                <div class="visual-island">
+                    <?php
+                    $arrow = "&#8596; <span class='secondary'>(same)</span>";
+                    if ($previous_scan) {
+                        if ($previous_scan->gpa > $context->gpa) {
+                            $arrow = "&#8595; <span class='secondary'>(worse)</span>";
+                        } else if ($previous_scan->gpa < $context->gpa) {
+                            $arrow = "&#8593; <span class='secondary'>(better)</span>";
+                        }
+    
+                        if ($site_pass_fail != $previous_scan->isPassFail()) {
+                            $arrow = "&#8800; <span class='secondary'>(incomparable)</span>";
+                        }
+                    }
+                    ?>
+                    <div class="dashboard-value">
+                        <?php echo $arrow ?>
+                    </div>
+                    <div class="dashboard-metric">Compared to Last Scan</div>
+                </div>
+            </div>
+            <div class="large-3 columns">
+                <div class="visual-island">
+                    <span class="dashboard-value"><?php echo $context->getABSNumberOfChanges() ?></span>
+                    <span class="dashboard-metric">Changes</span>
+                </div>
+            </div>
+            <div class="large-3 columns">
+                <div class="visual-island">
+                    <span class="dashboard-value"><?php echo $pages->count() ?></span>
+                    <span class="dashboard-metric">Pages</span>
+                </div>
             </div>
         </div>
     </section>
-
+    
     <section id="changes">
         <?php 
         if ($previous_scan) {
