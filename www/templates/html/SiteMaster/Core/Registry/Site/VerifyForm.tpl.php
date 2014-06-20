823738d61898ece96301bde10a59fdbffc78d4e1
Michael Fairchild <mfairchild365@gmail.com>
2014-06-20 10:09:33 -0500

    Clarify what 'verify' means.

--- www/templates/html/SiteMaster/Core/Registry/Site/VerifyForm.tpl.php
+++ www/templates/html/SiteMaster/Core/Registry/Site/VerifyForm.tpl.php
@@ -1,14 +1,24 @@
+<?php 
+$different_user = false;
+if ($context->verify_user->id != $context->current_user->id) {
+    $different_user = true;
+}
+?>
+
 <form action="<?php echo $context->getEditURL(); ?>" method="POST">
     <div class="panel">
         <p>
-            As a verified administrator, you will be able to add/remove and approve memberships, and edit site details for this site.
+            As a verified administrator, <?php echo $different_user?$context->verify_user->getName():'you' ?> will be able to add/remove and approve memberships, and edit site details for this site.  <strong>You do not need to verify this membership if <?php echo $different_user?$context->verify_user->getName().' does':' you do' ?> not need the admin role.</strong>
         </p>
         <p>
-            You will need to verify your membership as an administrator before your roles are active.  You have a few options:
+            <a href="<?php echo $context->site->getURL() ?>members/" class="button wdn-button">The admin role is not needed, Skip Verification</a>
+        </p>
+        <p>
+            You have a few options to verify the admin role for this membership:
             <ul>
                 <li>Upload a unique file to prove that you have physical access to the site</li>
-                <li>Have verified member of this site manually verify your membership</li>
-                <li>Have verified member of this site manually approve your individual roles</li>
+                <li>Have verified member of this site manually verify the membership</li>
+                <li>Have verified member of this site manually approve the individual roles</li>
             </ul>
         </p>
         
