e01ab7a46fc5cb37db27d13092396644fb7b0d3c
Michael Fairchild <mfairchild365@gmail.com>
2015-01-13 11:32:19 -0600

    Fix a blind copy/paste

--- src/SiteMaster/Core/Registry/Site/EditForm.php
+++ src/SiteMaster/Core/Registry/Site/EditForm.php
@@ -137,8 +137,8 @@ class EditForm implements ViewableInterface, PostHandlerInterface
             $this->site->support_email = $post['support_email'];
         }
 
-        if (isset($post['support_email'])) {
-            $this->site->support_email = $post['support_email'];
+        if (isset($post['support_groups'])) {
+            $this->site->support_groups = $post['support_groups'];
         }
         
         if (isset($post['production_status']) && in_array($post['production_status'], array(
