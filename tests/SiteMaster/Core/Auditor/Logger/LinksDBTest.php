a705e1700c6ee69f4bca3d4827ebc186a7859dca
Michael Fairchild <mfairchild365@gmail.com>
2015-02-18 14:53:45 -0600

    Improve tests for the link logger

--- tests/SiteMaster/Core/Auditor/Logger/LinksDBTest.php
+++ tests/SiteMaster/Core/Auditor/Logger/LinksDBTest.php
@@ -17,7 +17,10 @@ class LinksDBTest extends DBTestCase
     {
         $this->setUpDB();
 
-        $base_uri = 'http://www.test.com/';
+        $base_uri      = 'http://www.test.com/';
+        $redirect_url  = 'http://unlcms.unl.edu/university-communications/sitemaster/example-redirect-301';
+        $not_found_url = 'http://unlcms.unl.edu/university-communications/sitemaster/example-404';
+        $okay_url      = 'http://unlcms.unl.edu/university-communications/sitemaster/';
 
         //Get a test site
         $site = Site::getByBaseURL($base_uri);
@@ -40,22 +43,23 @@ class LinksDBTest extends DBTestCase
         $logger = new Logger\Links($spider, $page);
 
         //Set up the xpath
-        $html = file_get_contents(Util::getRootDir() . '/tests/data/multiple_sites.html');
+        $html = file_get_contents(Util::getRootDir() . '/tests/data/link_logger_test.html');
         $xpath = $parser->parse($html);
 
         //log
         $logger->log($base_uri, 1, $xpath);
 
         $expected_links = array(
-            'http://www.test.com/page2.html',
-            'http://www.test.com/test/page2.html'
+            $redirect_url,
+            $not_found_url,
+            $okay_url,
         );
         
         $list = new AllForPage(array('scanned_page_id'=>$page->id));
         
         $found_links = array();
         foreach ($list as $found_link) {
-            $found_links[] = $found_link->link_url;
+            $found_links[] = $found_link->original_url;
         }
 
         //Sort arrays to ensure order is the same. (order does not matter for this test)
@@ -64,8 +68,14 @@ class LinksDBTest extends DBTestCase
         
         $this->assertEquals($expected_links, $found_links, 'All links should be logged');
         
-        //TODO: change the name of multiple_sites.html or make a new page, with other external links that should be logged.
-
+        $redirect_link  = Page\Link::getByOriginalURL($redirect_url);
+        $not_found_link = Page\Link::getByOriginalURL($not_found_url);
+        $okay_link      = Page\Link::getByOriginalURL($okay_url);
+        
+        $this->assertEquals(301, $redirect_link->original_status_code);
+        $this->assertEquals(200, $redirect_link->final_status_code);
+        $this->assertEquals(404, $not_found_link->original_status_code);
+        $this->assertEquals(200, $okay_link->original_status_code);
     }
 
     public function setUpDB()
