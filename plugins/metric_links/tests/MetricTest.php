9a24a2dc3e7833ddfdcf269263844afc0e0183e8
Michael Fairchild <mfairchild365@gmail.com>
2015-02-18 16:36:07 -0600

    update the test to reflect the grading

--- plugins/metric_links/tests/MetricTest.php
+++ plugins/metric_links/tests/MetricTest.php
@@ -12,17 +12,17 @@ class MetricTest extends \PHPUnit_Framework_TestCase
         
         $metric->options['grading_method'] = Metric::GRADE_METHOD_DEFAULT;
         $this->assertEquals(20, $metric->getPointDeduction(404));
-        $this->assertEquals(5, $metric->getPointDeduction(''));
+        $this->assertEquals(0, $metric->getPointDeduction(''));
         $this->assertEquals(0, $metric->getPointDeduction(301));
         
         $metric->options['grading_method'] = Metric::GRADE_METHOD_NUMBER_OF_LINKS;
         $this->assertEquals(2, $metric->getPointDeduction(404));
-        $this->assertEquals(1, $metric->getPointDeduction(''));
+        $this->assertEquals(0, $metric->getPointDeduction(''));
         $this->assertEquals(0, $metric->getPointDeduction(301));
         
         $metric->options['grading_method'] = Metric::GRADE_METHOD_PASS_FAIL;
         $this->assertEquals(1, $metric->getPointDeduction(404));
-        $this->assertEquals(1, $metric->getPointDeduction(''));
+        $this->assertEquals(0, $metric->getPointDeduction(''));
         $this->assertEquals(0, $metric->getPointDeduction(301));
     }
 }
