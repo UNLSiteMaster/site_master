01fca7fbf770ff7a1ab795bfbedf834a9c623ed7
Michael Fairchild <mfairchild365@gmail.com>
2015-02-27 11:07:42 -0600

    Make the rescan button more readable

--- www/templates/html/SiteMaster/Core/Auditor/Site/Page/ScanForm.tpl.php
+++ www/templates/html/SiteMaster/Core/Auditor/Site/Page/ScanForm.tpl.php
@@ -3,7 +3,7 @@ if ($context->canEdit()) {
     ?>
     <form action="<?php echo $context->getEditURL(); ?>" method="POST">
         <input type="hidden" name="action" value="scan" />
-        <button type="submit" class="scan-page">Start a New Page Scan</button>
+        <button type="submit" class="scan-page">Rescan This Page</button>
     </form>
 <?php
 }
