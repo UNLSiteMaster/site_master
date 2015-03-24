b7cd25772ac2ae00c9b1ab3f68989a5e4155dabc
Michael Fairchild <mfairchild365@gmail.com>
2015-03-24 14:03:32 -0500

    Improve the query
    Use aggregate functions to order the query.
    Use class constants instead of magic values.
    Only select pages that are queued.

--- src/SiteMaster/Core/Auditor/Scans/Queued.php
+++ src/SiteMaster/Core/Auditor/Scans/Queued.php
@@ -1,5 +1,7 @@
 <?php
 namespace SiteMaster\Core\Auditor\Scans;
+use SiteMaster\Core\Auditor\Scan;
+use SiteMaster\Core\Auditor\Site\Page;
 
 /**
  * This class will compile a list of all scans that are queued, in the order in which they will be processed.
@@ -17,12 +19,13 @@ class Queued extends All
     public function getSQL()
     {
         //Build the list
-        $sql = "SELECT scans.id as id
+        $sql = "SELECT scans.id as id, MIN(scanned_page.priority) as priority, MIN(scanned_page.date_created) as date_created
                 FROM scanned_page
                 LEFT JOIN scans ON (scanned_page.scans_id = scans.id)
-                WHERE scans.status = 'QUEUED'
+                WHERE scans.status = '" . Scan::STATUS_QUEUED . "'
+                    AND scanned_page.status = '" . Page::STATUS_QUEUED ."'
                 GROUP BY scans.id
-                ORDER BY scanned_page.priority ASC, scanned_page.date_created ASC";
+                ORDER BY priority ASC, date_created ASC";
 
         return $sql;
     }
