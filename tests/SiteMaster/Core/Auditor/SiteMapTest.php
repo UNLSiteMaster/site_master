da53bd59bd84ff887a4be7ffdcf3f1a43cdc0c1e
Michael Fairchild <mfairchild365@gmail.com>
2015-03-25 09:38:04 -0500

    Add a site map parser

---
+++ tests/SiteMaster/Core/Auditor/SiteMapTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace SiteMaster\Core\Auditor;
+
+class SiteMapTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function getURLs()
+    {
+        $site_map = new SiteMap(__DIR__ . '/../../../data/sample_site_map.xml');
+        
+        $expected_URLs = array(
+            'http://www.example.com/',
+            'http://www.example.com/page1',
+            'http://www.example.com/page2',
+            'http://www.example.com/page3',
+            'http://www.example.com/page4',
+        );
+        
+        $this->assertEquals($expected_URLs, $site_map->getURLs());
+    }
+}
