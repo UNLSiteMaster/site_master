79dd60102d204ce02957f52aef958d392feb85a1
Michael Fairchild <mfairchild365@gmail.com>
2015-03-31 14:03:13 -0500

    Improve the index on the scanned_page_links table
    When there is a large amount of data, this can improve the speed of scans by about 10x.

--- data/update-2015032301.sql
+++ data/update-2015032301.sql
@@ -1,3 +1,4 @@
 ALTER TABLE sites ADD site_map_url VARCHAR(2100);
 ALTER TABLE sites ADD crawl_method ENUM('CRAWL_ONLY', 'SITE_MAP_ONLY', 'HYBRID') NOT NULL DEFAULT 'HYBRID';
 ALTER TABLE scanned_page ADD found_with ENUM('SITE_MAP', 'CRAWL') NOT NULL DEFAULT 'CRAWL';
+ALTER TABLE scanned_page_links ADD INDEX scanned_page_links_last_uncached (date_created, scanned_page_id, original_url_hash, cached)
