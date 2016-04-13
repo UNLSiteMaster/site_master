d0bfa2ed23cdca5a6f1325c9c4c3fe10c3d05245
Michael Fairchild <mfairchild365@gmail.com>
2016-04-13 16:13:59 -0500

    Use a better HTML5 parser
    Rather than using tidy and converting to xhtml etc, just use an html5 specific parser. This resolves several outstanding issues with converting html5 to xhtml, which in some cases was causing pages to not be processed.

--- src/SiteMaster/Core/Auditor/Parser/HTML5.php
+++ src/SiteMaster/Core/Auditor/Parser/HTML5.php
@@ -5,13 +5,6 @@ class HTML5 extends \Spider_Parser
 {
     public function __construct($options = array())
     {
-        $options = array_replace_recursive($options, array(
-            'tidy_config' => array(
-                'new-blocklevel-tags' => 'article,header,footer,section,nav,main,aside,figure,figcaption',
-                'new-inline-tags'     => 'video,audio,canvas,ruby,rt,rp,track,mark,meter,time',
-            )
-        ));
-        
         parent::__construct($options);
     }
 
@@ -22,24 +15,13 @@ class HTML5 extends \Spider_Parser
 
         $content = html_entity_decode($content, ENT_HTML5);
 
-        if ($this->options['tidy'] && extension_loaded('tidy')) {
-            //Convert and repair as xhtml
-            $tidy     = new \tidy;
-            $content = $tidy->repairString($content, $this->options['tidy_config']);
-        }
-
         if (function_exists('mb_convert_encoding')) {
             //Ensure content is UTF-8, if it isn't, loadXML might not work.
             $content = mb_convert_encoding($content, 'UTF-8');
         }
-
-        //Skip application/ld+json scripts as they break libxml. TODO: don't use libxml. :p
-        $content = preg_replace('/<script type=\"application\/ld\+json\">.*?<\/script>/s','<!-- script application/ld+json removed by sitemaster -->',$content);
-
-        $document->loadXML(
-            $content,
-            LIBXML_NOERROR | LIBXML_NONET | LIBXML_NOWARNING | LIBXML_NOCDATA
-        );
+        
+        $html5 = new \Masterminds\HTML5();
+        $document = $html5->loadHTML($content);
 
         $xpath = new \DOMXPAth($document);
         $xpath->registerNamespace('xhtml', 'http://www.w3.org/1999/xhtml');
