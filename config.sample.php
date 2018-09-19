efb8ec021089815575ffa2080b5b627fc29fe508
Jesus Ortiz <jesusotovar@hotmail.com>
2018-09-19 08:40:24 -0600

    Enabling axe and w3c validator

--- config.sample.php
+++ config.sample.php
@@ -1,47 +1,39 @@
 <?php
 use SiteMaster\Core\Config;
-
 /**********************************************************************************************************************
  * php related settings
  */
-
 ini_set('display_errors', true);
-
 error_reporting(E_ALL);
-
 Config::set('URL', '/'); //Trailing slash is important
-
 /**********************************************************************************************************************
  * DB related settings
  */
-Config::set('DB_HOST'     , 'localhost');
-Config::set('DB_USER'     , 'sitemaster');
-Config::set('DB_PASSWORD' , 'password');
-Config::set('DB_NAME'     , 'sitemaster');
-
+Config::set('DB_HOST', 'localhost');
+Config::set('DB_USER', 'sitemaster');
+Config::set('DB_PASSWORD', 'password');
+Config::set('DB_NAME', 'sitemaster');
 /**********************************************************************************************************************
  * Other settings, including theme
  */
 Config::set('THEME', 'foundation');
-
-
 /**********************************************************************************************************************
  * Plugin related settings
  */
 Config::set('PLUGINS', array(
-    'example' => array('setting'=>'value'),
-    'theme_foundation' => array('setting'=>'value'),
+    'metric_axe' => array('setting' => 'value'),
+    'metric_w3c_html' => array('setting' => 'value'),
+    'theme_foundation' => array('setting' => 'value'),
     'auth_google' => array(
-        'security_salt' => 'enter random string here',
-            'Strategy' => array(
-                'Google' => array(
-                    'client_id' => 'enter client id here',
-                    'client_secret' => 'enter client secret here'
-                )
+        'security_salt' => 'taco',
+        'Strategy' => array(
+            'Google' => array(
+                'client_id' => '',
+                'client_secret' => ''
             )
+        )
     )
 ));
-
 /**********************************************************************************************************************
  * group settings
  */
@@ -53,34 +45,34 @@ Config::set('GROUPS', [
             'example' => array(
                 'setting' => 'value',
             ),
+            'metric_axe' => [],
+            'metric_w3c_html' => [],
         ],
         //You can also set a few other settings on a group level
         //'SITE_PASS_FAIL' => false,
         //'SCAN_PAGE_LIMIT' => 5,
     ],
 ]);
-
 /**********************************************************************************************************************
  * unit test settings
  */
-Config::set('TEST_DB_HOST'     , 'localhost');
-Config::set('TEST_DB_USER'     , 'sitemaster_test');
-Config::set('TEST_DB_PASSWORD' , 'password');
-Config::set('TEST_DB_NAME'     , 'sitemaster_test');
-
+Config::set('TEST_DB_HOST', 'localhost');
+Config::set('TEST_DB_USER', 'sitemaster_test');
+Config::set('TEST_DB_PASSWORD', 'password');
+Config::set('TEST_DB_NAME', 'sitemaster_test');
 /**********************************************************************************************************************
  * TRAVIS settings
  */
 if (getenv('TRAVIS')) {
     //
-    Config::set('DB_HOST'     , '127.0.0.1');
-    Config::set('DB_USER'     , 'travis');
-    Config::set('DB_PASSWORD' , '');
-    Config::set('DB_NAME'     , 'sitemaster_test');
+    Config::set('DB_HOST', '127.0.0.1');
+    Config::set('DB_USER', 'travis');
+    Config::set('DB_PASSWORD', '');
+    Config::set('DB_NAME', 'sitemaster_test');
     
     //Set  the config to match the production config for travis CI
-    Config::set('TEST_DB_HOST'     , Config::get('DB_HOST'));
-    Config::set('TEST_DB_USER'     , Config::get('DB_USER'));
-    Config::set('TEST_DB_PASSWORD' , Config::get('DB_PASSWORD'));
-    Config::set('TEST_DB_NAME'     , Config::get('DB_NAME'));
+    Config::set('TEST_DB_HOST', Config::get('DB_HOST'));
+    Config::set('TEST_DB_USER', Config::get('DB_USER'));
+    Config::set('TEST_DB_PASSWORD', Config::get('DB_PASSWORD'));
+    Config::set('TEST_DB_NAME', Config::get('DB_NAME'));
 }
