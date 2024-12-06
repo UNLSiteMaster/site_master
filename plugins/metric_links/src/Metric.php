142145311d02559186936f8ae00acec1da85c494
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2024-12-06 14:13:16 -0600

    Fix redirect links (#209)
    * Add checks for status codes after redirect
    
    * Added new marks types for errors after redirect
    
    * Fix typo
    
    * Updated mark text

--- plugins/metric_links/src/Metric.php
+++ plugins/metric_links/src/Metric.php
@@ -41,7 +41,7 @@ class Metric extends MetricInterface
      * 4**, 5** and connection errors will cause the metric to fail
      */
     const GRADE_METHOD_PASS_FAIL = 3;
-    
+
     const MARK_LINK_LIMIT_HIT = 'mark_link_limit_hit';
 
     /**
@@ -67,7 +67,7 @@ class Metric extends MetricInterface
             'http_error_codes' => array(
                 301,
                 400, 402, 403, 404,
-                500, 501, 502, 503 //500 level errors are included because they impose a bad user experience, and appear 'broken' to end users 
+                500, 501, 502, 503 //500 level errors are included because they impose a bad user experience, and appear 'broken' to end users
             ),
             'message_text' => array(
                 'link_connection_error_3' => 'The URL is malformed',
@@ -83,6 +83,14 @@ class Metric extends MetricInterface
                 'link_http_code_501' => 'Not Implemented (501)',
                 'link_http_code_502' => 'Bad Gateway (502)',
                 'link_http_code_503' => 'Service Unavailable (503)',
+                'link_http_code_after_redirect_400' => 'Bad Request, After Redirect (400)',
+                'link_http_code_after_redirect_402' => 'Payment Required, After Redirect (402)',
+                'link_http_code_after_redirect_403' => 'Forbidden, After Redirect (403)',
+                'link_http_code_after_redirect_404' => 'Not Found, After Redirect (404)',
+                'link_http_code_after_redirect_500' => 'Internal Server Error, After Redirect (500)',
+                'link_http_code_after_redirect_501' => 'Not Implemented, After Redirect (501)',
+                'link_http_code_after_redirect_502' => 'Bad Gateway, After Redirect (502)',
+                'link_http_code_after_redirect_503' => 'Service Unavailable, After Redirect (503)',
                 self::MARK_LINK_LIMIT_HIT => 'Link limit was hit, not all links were scanned',
             ),
             'help_text' => array(
@@ -99,11 +107,19 @@ class Metric extends MetricInterface
                 'link_http_code_501' => 'Not Implemented',
                 'link_http_code_502' => 'Bad Gateway',
                 'link_http_code_503' => 'This will usually get resolved without any need for action on your part.  If not, you will have to contact the server administrator or remove this link.',
+                'link_http_code_after_redirect_400' => '(After the redirect) The remote server did not understand the link.  Either fix the link or remove it.',
+                'link_http_code_after_redirect_402' => '(After the redirect) Payment Required',
+                'link_http_code_after_redirect_403' => '(After the redirect) The content that this link points to requires authorization to access.  Please ensure that this is not a mistake and that there is enough context to help the user gain access if they need to.',
+                'link_http_code_after_redirect_404' => '(After the redirect) The content that this link points to no longer exists.  Please remove this link.',
+                'link_http_code_after_redirect_500' => '(After the redirect) The server is returning an error for this link.  This may be resolved in time without any action on your part, but it might be worth while to contact the server\'s administrator or remove/update this link.',
+                'link_http_code_after_redirect_501' => '(After the redirect) Not Implemented',
+                'link_http_code_after_redirect_502' => '(After the redirect) Bad Gateway',
+                'link_http_code_after_redirect_503' => '(After the redirect) This will usually get resolved without any need for action on your part.  If not, you will have to contact the server administrator or remove this link.',
                 self::MARK_LINK_LIMIT_HIT => 'The link limit was hit, so not all links on the page were scanned. You will have to manually check links on the page.',
             ),
             'suppressed_domains' => $suppressed_domains,
         ), $options);
-        
+
         parent::__construct($plugin_name, $options);
     }
 
@@ -149,7 +165,7 @@ class Metric extends MetricInterface
         if ($this->options['grading_method'] == self::GRADE_METHOD_PASS_FAIL) {
             return true;
         }
-        
+
         return false;
     }
 
@@ -168,19 +184,19 @@ class Metric extends MetricInterface
     public function scan($uri, \DOMXPath $xpath, $depth, Page $page, Metrics $context)
     {
         $this->markPage($page);
-        
+
         return true;
     }
 
     /**
      * This method will find broken links and mark a page appropriately
-     * 
+     *
      * @param Page $page the page to mark
      */
     public function markPage(Page $page)
     {
         $links = $page->getLinks();
-        
+
         foreach ($links as $link) {
 
             // Ignore suppressed domains
@@ -188,33 +204,51 @@ class Metric extends MetricInterface
                 continue;
             }
 
-            if (!$this->isError($link)) {
-                //don't mark it...
-                continue;
-            }
+            // If we get an error after the redirect
+            if ($link->isRedirect() && $this->isFinalError($link)) {
+                $machine_name = $this->getMachineNameForFinalStatus($link);
+                $message      = $this->getStatusMessage($machine_name);
+                $help_text    = $this->getStatusHelpText($machine_name);
+                $points       = $this->getPointDeduction($link->final_status_code);
 
-            $machine_name = $this->getMachineNameForStatus($link);
-            $message      = $this->getStatusMessage($machine_name);
-            $help_text    = $this->getStatusHelpText($machine_name);
-            $points       = $this->getPointDeduction($link->original_status_code);
+                $allows_perm_override = false;
+                if ($points === 0) {
+                    //Allow notices for this metric to be permanently overridden, because they likely do not need to be reviewed again.
+                    $allows_perm_override = true;
+                }
+                $mark = $this->getMark($machine_name, $message, $points, null, $help_text, $allows_perm_override);
 
-            $allows_perm_override = false;
-            if ($points === 0) {
-                //Allow notices for this metric to be permanently overridden, because they likely do not need to be reviewed again.
-                $allows_perm_override = true;
-            }
-            $mark = $this->getMark($machine_name, $message, $points, null, $help_text, $allows_perm_override);
+                $value_found = $link->original_url . ' which redirected to ' . $link->final_url;
 
-            $value_found = $link->original_url;
-            if ($link->isRedirect()) {
-                $value_found .= ' which redirected to ' . $link->final_url;
+                $page->addMark($mark, array(
+                    'value_found' => $value_found
+                ));
+
+            // If we had a non-200 status code
+            } elseif ($this->isOriginalError($link)) {
+                $machine_name = $this->getMachineNameForOriginalStatus($link);
+                $message      = $this->getStatusMessage($machine_name);
+                $help_text    = $this->getStatusHelpText($machine_name);
+                $points       = $this->getPointDeduction($link->original_status_code);
+
+                $allows_perm_override = false;
+                if ($points === 0) {
+                    //Allow notices for this metric to be permanently overridden, because they likely do not need to be reviewed again.
+                    $allows_perm_override = true;
+                }
+                $mark = $this->getMark($machine_name, $message, $points, null, $help_text, $allows_perm_override);
+
+                $value_found = $link->original_url;
+                if ($link->isRedirect()) {
+                    $value_found .= ' which redirected to ' . $link->final_url;
+                }
+
+                $page->addMark($mark, array(
+                    'value_found' => $value_found
+                ));
             }
-            
-            $page->addMark($mark, array(
-                'value_found' => $value_found
-            ));
         }
-        
+
         if (Page::LIMIT_LIMIT_HIT_YES == $page->link_limit_hit) {
             //add a notice that we did not check all of the links on the page
             $message = $this->getStatusMessage(self::MARK_LINK_LIMIT_HIT);
@@ -228,7 +262,7 @@ class Metric extends MetricInterface
 
     /**
      * get the message for a status to be used with a mark
-     * 
+     *
      * @param string $machine_name the machine name of the mark
      * @return string
      */
@@ -237,13 +271,13 @@ class Metric extends MetricInterface
         if (isset($this->options['message_text'][$machine_name])) {
             return $this->options['message_text'][$machine_name];
         }
-        
+
         return 'General Connection error';
     }
 
     /**
      * get the help text to be used with a mark for a given machine name
-     * 
+     *
      * @param string $machine_name the machine name of the mark
      * @return string
      */
@@ -257,22 +291,42 @@ class Metric extends MetricInterface
     }
 
     /**
-     * Determine if a status is an error and should be logged
+     * Determine if an original status is an error and should be logged
      *
      * @param Page\Link $link
      * @internal param Page\Link $status
      * @return bool
      */
-    public function isError(Page\Link $link)
+    public function isOriginalError(Page\Link $link)
     {
         if ($link->isCurlError()) {
             return true;
         }
-        
+
         if (in_array($link->original_status_code, $this->options['http_error_codes'])) {
             return true;
         }
-        
+
+        return false;
+    }
+
+    /**
+     * Determine if a final status after redirect is an error and should be logged
+     *
+     * @param Page\Link $link
+     * @internal param Page\Link $status
+     * @return bool
+     */
+    public function isFinalError(Page\Link $link)
+    {
+        if ($link->isCurlError()) {
+            return true;
+        }
+
+        if (in_array($link->final_status_code, $this->options['http_error_codes'])) {
+            return true;
+        }
+
         return false;
     }
 
@@ -302,19 +356,35 @@ class Metric extends MetricInterface
     }
 
     /**
-     * Get the machine name for a status
+     * Get the machine name for a original status
      *
      * @param Page\Link $link
      * @internal param Page\Link $status
      * @return string
      */
-    public function getMachineNameForStatus(Page\Link $link)
+    public function getMachineNameForOriginalStatus(Page\Link $link)
     {
         if ($link->isCurlError()) {
             return 'link_connection_error_' . $link->original_curl_code;
         }
-        
+
         return 'link_http_code_' . $link->original_status_code;
+    }
+
+    /**
+     * Get the machine name for a final status after the redirect
+     *
+     * @param Page\Link $link
+     * @internal param Page\Link $status
+     * @return string
+     */
+    public function getMachineNameForFinalStatus(Page\Link $link)
+    {
+        if ($link->isCurlError()) {
+            return 'link_connection_error_' . $link->final_curl_code;
+        }
+
+        return 'link_http_code_after_redirect_' . $link->final_status_code;
     }
 
     /**
@@ -335,12 +405,12 @@ class Metric extends MetricInterface
                     //These can be legitimate, show them as a notice
                     return 0;
                 }
-                
+
                 if ($http_code >= 400) {
                     //error
                     return 20;
                 }
-                
+
 
                 //Connection problems (zero points because it is probably our fault)
                 return 0;
@@ -354,7 +424,7 @@ class Metric extends MetricInterface
                     //These can be legitimate, show them as a notice
                     return 0;
                 }
-                
+
                 if ($http_code >= 400) {
                     //error
                     return 2;
@@ -377,12 +447,12 @@ class Metric extends MetricInterface
                     //These can be legitimate, show them as a notice
                     return 0;
                 }
-                
+
                 if ($http_code == 0) {
                     //Connection problems (zero points because it is probably our fault)
                     return 0;
                 }
-                
+
                 return 1;
         }
     }
