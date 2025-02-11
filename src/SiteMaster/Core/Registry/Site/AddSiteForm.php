30cfb5ee513dc2e89d8460298ef7363245ac9700
Thomas Neumann <thomasneumann21800@gmail.com>
2025-02-11 14:37:41 -0600

    Fix url encoding

--- src/SiteMaster/Core/Registry/Site/AddSiteForm.php
+++ src/SiteMaster/Core/Registry/Site/AddSiteForm.php
@@ -120,7 +120,7 @@ class AddSiteForm extends AbstractPostHandler implements ViewableInterface
     public function getEditURL()
     {
         if (isset($_GET['recommended']) && !empty($_GET['recommended'])) {
-            return $this->getURL() . "?recommended=" . $_GET['recommended'];
+            return $this->getURL() . "?recommended=" . urlencode($_GET['recommended']);
         }
 
         return $this->getURL();
